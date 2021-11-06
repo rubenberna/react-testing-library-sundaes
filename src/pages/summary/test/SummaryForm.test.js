0ee7fd012998a73b0a6cb9bc8eeac6f427ae9e78
rubenberna <rubenmbernardes@gmail.com>
2021-11-06 16:45:07 +0100

    first test

---
+++ src/pages/summary/test/SummaryForm.test.js
@@ -0,0 +1,28 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { SummaryForm } from "../SummaryForm";
+
+test("initial conditions", () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test("checkbox enables/disables button", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
